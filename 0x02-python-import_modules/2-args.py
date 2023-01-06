a2eee993523bd703a3e925aad61787cda1dd8286
princebeyond <amoodeyond222@gmail.com>
2023-01-06 12:13:06 +0100

    life

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 
     arg_len = len(sys.argv) - 1
     if arg_len == 0:
-        print('{} arguments'.format(arg_len))
+        print('{} arguments.'.format(arg_len))
     elif arg_len == 1:
         print('{} argument:'.format(arg_len))
         print('1: {}'.format(str(sys.argv[1])))
