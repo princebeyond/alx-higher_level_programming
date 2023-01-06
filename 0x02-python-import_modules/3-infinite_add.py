1984ac192ae87c97cfe05c614ead604bdea129f2
princebeyond <amoodeyond222@gmail.com>
2023-01-06 12:37:53 +0100

    life

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 if __name__ == '__main__':
     import sys
     arg_sum = 0
-    arg_len = len(sys.arg)
+    arg_len = len(sys.argv)
 
     for i in range(1, arg_len):
         arg_sum += int(sys.argv[i])
-        print('{:d}'.format(arg_sum))
+    print('{:d}'.format(arg_sum))
