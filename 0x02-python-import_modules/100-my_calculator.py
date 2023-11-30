a7d7c4f4fe2dcf7bb398155f5ba4b0bf1e6b4549
princebeyond <amoodeyond222@gmail.com>
2023-11-30 11:59:21 -0800

    test2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,16 +4,16 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
 
     if len(sys.argv) < 3:
-         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-         sys.exit(1)
-    
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     operator = (sys.argv[2])
     b = int(sys.argv[3])
 
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif  operator == "-":
+    elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
