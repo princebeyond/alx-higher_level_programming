fd9abe6532baf2ac0aca73d9ad97a7f7bc91701e
princebeyond <amoodeyond222@gmail.com>
2023-11-30 05:07:47 -0800

    test3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-length = (len(sys.argv) - 1)
-if length == 0:
-    print("{} arguments.".format(length))
-elif length == 1:
-    print("{} argument:".format(length))
-else:
-    print("{} arguments:".format(length))
-for i in range(1, length + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+    length = (len(sys.argv) - 1)
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
