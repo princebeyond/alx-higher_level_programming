4ceb92333471d38c6bac15bc750cb2d10c09ee53
princebeyond <amoodeyond222@gmail.com>
2023-12-04 04:49:15 -0800

    test2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == ():
+    if not sentence:
         # checking if tuple is empty with () list is []
-        return None
+        return 0, None
     else:
         # returns 2 at once
         return len(sentence), sentence[0]
