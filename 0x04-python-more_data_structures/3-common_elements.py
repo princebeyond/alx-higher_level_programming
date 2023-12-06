244dc230cf111a1f7a4681175d8ec688a2e18085
princebeyond <amoodeyond222@gmail.com>
2023-12-06 06:54:20 -0800

    test1

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # setting it to set so it will be able to get
+    # the charater that in both wen returning it
+    first = set(set_1)
+    # intersection of two sets
+    # return to the boss which is main
+    return first & set_2
