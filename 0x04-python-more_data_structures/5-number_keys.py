176f9ef01e0317dd9e8093b0c254d8de265733a7
princebeyond <amoodeyond222@gmail.com>
2023-12-06 07:26:15 -0800

    test3

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     # add key to the dict list
-    a_dictionary['key'] = 3
+    # a_dictionary['key'] = 3
     # and return it
-    return a_dictionary.get('key')
+    # return a_dictionary.get('key')
+    # i was supposed to get the length not to add it in there soo.......
+    return len(a_dictionary)
