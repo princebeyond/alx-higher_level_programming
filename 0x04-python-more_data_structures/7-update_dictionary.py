8eca69478cf355c9d0a5da025ec959818df9e167
princebeyond <amoodeyond222@gmail.com>
2023-12-06 11:05:36 -0800

    test1

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # my commented code was my doing but turn out this ain't
+    # the correct code ai helped me clear all my code nd that was correct
+    # lol twas fun sha
+    # for i in a_dictionary:
+    # if a_dictionary[i] == "C":
+    # del a_dictionary['language']
+    # a_dictionary[key] = value
+    # break
+    a_dictionary[key] = value
+    return a_dictionary
