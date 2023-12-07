06fd36136427fbe5e253eee0e9841a0c42e1d1ae
princebeyond <amoodeyond222@gmail.com>
2023-12-06 20:09:48 -0800

    test1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,12 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     # getting the maximum value in dict and the key to it
-    maximuim_key = max(a_dictionary, key=a_dictionary.get)
+    # maximuim_key = max(a_dictionary, key=a_dictionary.get)
     # returns maximuim_key
-    return maximuim_key
+    maxval = 0
+    maxkey = None
+    for k, v in a_dictionary.items():
+        if v > maxval:
+            maxval = v
+            maxkey = k
+    return maxkey
