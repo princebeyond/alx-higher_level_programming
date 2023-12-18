ad94dd701331f906c08e85a109ffc51dcfb962f7
princebeyond <amoodeyond222@gmail.com>
2023-12-18 03:56:48 -0800

    test1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    # using try blocks
     try:
-        # checking if value is int
         check = int(value)
-        # printing value that is int
         print("{:d}".format(check))
-        # returns true if vaue is int
         return True
-    # handling thr valueerror it make if not
     except ValueError:
-        # returns false if value not int
         return False
