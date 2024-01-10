d69214b9b45251c5daed5d60327ae8931907619b
princebeyond <amoodeyond222@gmail.com>
2024-01-10 09:00:56 -0800

    test1

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@ T of F
 
 
 def is_kind_of_class(obj, a_class):
-    """if the object is an instance of, or if the object is an instance of a class"""
+    """if the object is an instance"""
     if not isinstance(obj, a_class):
         return False
     else:
