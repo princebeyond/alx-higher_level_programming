31d81ffa206c416f2645c6fd991816d3b862af74
princebeyond <amoodeyond222@gmail.com>
2024-01-09 20:27:28 -0800

    test2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the read kines
+"""
+
 
 def read_file(filename=""):
+    """Reads file content"""
     with open(filename, "r") as f:
-        print(f.read())
+        print(f.read(), end="")
