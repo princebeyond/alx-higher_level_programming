31bfa19f8530b27a3d2afd951af474e743f94cc9
princebeyond <amoodeyond222@gmail.com>
2024-01-10 10:23:47 -0800

    test1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Define area"""
+    def area(self):
+        """initializing empty area"""
+        raise Exception("area() is not implemented")
