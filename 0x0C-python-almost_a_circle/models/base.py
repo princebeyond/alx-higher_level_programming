2c2587c7c8da3d27a73e5ae2d1483e0a4ae6138c
princebeyond <amoodeyond222@gmail.com>
2024-01-15 23:50:43 -0800

    test8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,7 +12,7 @@ import turtle
 class Base:
     """Represent Base model"""
 
-    __nb_object = 0
+    __nb_objects = 0
 
 
     def __init__(self, id=None):
@@ -20,5 +20,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.___nb_objects += 1
-            self.id = Base.___nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,9 +21,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError: ("width must be an integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError:("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -34,9 +34,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError: ("height must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError: ("height must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -47,9 +47,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
-            raise TypeError: ("x must be an integer")
-        if value <= 0:
-            raise ValueError:("x must be > 0")
+            raise TypeError ("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -60,9 +60,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
-            raise TypeError: ("y must be an integer")
-        if value <= 0:
-            raise ValueError:("y must be > 0")
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
 
     def area(self):
