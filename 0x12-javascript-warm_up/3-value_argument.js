0f07de2dcfcfe307f58d779fe084e1a53430ca23
princebeyond <amoodeyond222@gmail.com>
2024-03-11 14:19:55 -0700

    PrintsTheFirstArgumentPassedToIt

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,17 +2,13 @@
 const { argv } = require('node:process');
 let i;
 let flag = false;
-for (i of argv)
-{
-	if (i == argv[2])
-	{
-		flag = true;
-	}
+for (i of argv) {
+  if (i === argv[2]) {
+    flag = true;
+  }
 }
-if (flag == true)
-{
-	console.log(argv[2]);
-} else
-{
-	console.log('No argument');
+if (flag === true) {
+  console.log(argv[2]);
+} else {
+  console.log('No argument');
 }
